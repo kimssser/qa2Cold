b254c526f162ae0a01bf224d01664cd0eadc0971
kimssser <skey2@inbox.lv>
2018-10-18 16:01:00 +0300

    test commit

--- src/test/java/ListClassTest.java
+++ src/test/java/ListClassTest.java
@@ -27,4 +27,6 @@ public class ListClassTest {
             System.out.println(name);
         }
     }
+
+
 }
--- src/test/java/SecondHomeWorkJUnit.java
+++ src/test/java/SecondHomeWorkJUnit.java
@@ -10,16 +10,19 @@ import java.util.List;
 public class SecondHomeWorkJUnit {
     private final By ARTICLE_TITLE = By.xpath(".//a[@class = 'top2012-title']");
     private final By M_ARTICLE_TITLE = By.xpath(".//a[@class='md-scrollpos']");
-    private final List<String> givenArticles = new ArrayList<String>();
+    private final List<String> givenArticlesList = new ArrayList<String>();
+    private final List<String> mobArticlesList = new ArrayList<String>();
+    private final List<String> deskArticlesList = new ArrayList<String>();
+
 
     @Test
     public void checkDelfiTestDesktopBrowsers() {
 
-        givenArticles.add("Esam spēruši platu soli, lai Latvijā varētu izveidot valdību, lepojas 'Attīstībai/Par!'");
-        givenArticles.add("'OlyBet' basketbola līga: 'Ogre' - 'Jēkabpils/SMScredit.lv'. Video tiešraide");
-        givenArticles.add("KP piedāvātais modelis vairs nav iespējams, norāda Dzintars");
-        givenArticles.add("Slaktiņu Krimas koledžā sarīkojis tās audzēknis");
-        givenArticles.add("Mūžībā devies mūziķis un LTV raidījuma vadītājs Valters Frīdenbergs");
+        givenArticlesList.add("Esam spēruši platu soli, lai Latvijā varētu izveidot valdību, lepojas 'Attīstībai/Par!'");
+        givenArticlesList.add("'OlyBet' basketbola līga: 'Ogre' - 'Jēkabpils/SMScredit.lv'. Video tiešraide");
+        givenArticlesList.add("KP piedāvātais modelis vairs nav iespējams, norāda Dzintars");
+        givenArticlesList.add("Slaktiņu Krimas koledžā sarīkojis tās audzēknis");
+        givenArticlesList.add("Mūžībā devies mūziķis un LTV raidījuma vadītājs Valters Frīdenbergs");
         //Letting know the system where to find our driver
         System.setProperty("webdriver.gecko.driver", "c:/geckodriver.exe");
         //Creating a new copy of driver to work with - opening Web Browser
@@ -29,19 +32,25 @@ public class SecondHomeWorkJUnit {
         //Open the page
         driver.get("http://delfi.lv");
         //Find 5 elements
-        List<WebElement> articles = driver.findElements(ARTICLE_TITLE);
+        List<WebElement> articlesWeb = driver.findElements(ARTICLE_TITLE);
         //Parbaude
         for (int i = 0; i < 5; i++) {
-            Assertions.assertEquals(givenArticles.get(i), articles.get(i).getText(), "Title Nr. " + (i + 1) + " is not correct!");
+            deskArticlesList.add(articlesWeb.get(i).getText());
+            System.out.println(deskArticlesList);
         }
+
+        for (int i = 0; i < 5; i++) {
+            Assertions.assertEquals(givenArticlesList.get(i), deskArticlesList.get(i), "Title Nr. " + (i + 1) + " is not correct!");
+        }
+
     }
     @Test
     public void checkDelfiTestMobileBrowsers() {
-        givenArticles.add("Esam spēruši platu soli, lai Latvijā varētu izveidot valdību, lepojas 'Attīstībai/Par!'");
-        givenArticles.add("'OlyBet' basketbola līga: 'Ogre' - 'Jēkabpils/SMScredit.lv'. Video tiešraide");
-        givenArticles.add("KP piedāvātais modelis vairs nav iespējams, norāda Dzintars");
-        givenArticles.add("Slaktiņu Krimas koledžā sarīkojis tās audzēknis");
-        givenArticles.add("Mūžībā devies mūziķis un LTV raidījuma vadītājs Valters Frīdenbergs");
+        givenArticlesList.add("Esam spēruši platu soli, lai Latvijā varētu izveidot valdību, lepojas 'Attīstībai/Par!'");
+        givenArticlesList.add("'OlyBet' basketbola līga: 'Ogre' - 'Jēkabpils/SMScredit.lv'. Video tiešraide");
+        givenArticlesList.add("KP piedāvātais modelis vairs nav iespējams, norāda Dzintars");
+        givenArticlesList.add("Slaktiņu Krimas koledžā sarīkojis tās audzēknis");
+        givenArticlesList.add("Mūžībā devies mūziķis un LTV raidījuma vadītājs Valters Frīdenbergs");
         //Letting know the system where to find our driver
         System.setProperty("webdriver.gecko.driver", "c:/geckodriver.exe");
         //Creating a new copy of driver to work with - opening Web Browser
@@ -51,12 +60,38 @@ public class SecondHomeWorkJUnit {
         //Open the page
         driver.get("http://m.delfi.lv");
         //Find 5 elements
-        WebElement mobArticle = driver.findElement(M_ARTICLE_TITLE);
-        //Getting text only from web element(Elements contains not oly the text but tags also)
-        List<WebElement> mobArticles = driver.findElements(M_ARTICLE_TITLE);
+        List<WebElement> mobArticlesWeb = driver.findElements(M_ARTICLE_TITLE);
+        //Parbaude
+
+        for (int i = 0; i < 5; i++) {
+            mobArticlesList.add(mobArticlesWeb.get(i).getText());
+        }
+        System.out.println(mobArticlesList);
+        for (int i = 0; i < 5; i++) {
+            Assertions.assertEquals(givenArticlesList.get(i), mobArticlesList.get(i), "Title Nr. " + (i + 1) + " is not correct!");
+        }
+    }
+
+    @Test
+    public void compareArticles () {
+
         //Parbaude
         for (int i = 0; i < 5; i++) {
-            Assertions.assertEquals(givenArticles.get(i), mobArticles.get(i).getText(), "Title Nr. " + (i + 1) + " is not correct!");
+            Assertions.assertEquals(deskArticlesList, mobArticlesList, "Title Nr. " + (i + 1) + " is not equal!");
         }
+        System.out.println(deskArticlesList);
+        System.out.println(mobArticlesList);
+
+    }
+
+    @Test
+    public void checkDesktopArticles () {
+
+        //Parbaude
+        for (int i = 0; i < 5; i++) {
+            Assertions.assertEquals(deskArticlesList, givenArticlesList, "Title Nr. " + (i + 1) + " is not equal!");
+        }
+        System.out.println(deskArticlesList);
+        System.out.println(givenArticlesList);
     }
 }
