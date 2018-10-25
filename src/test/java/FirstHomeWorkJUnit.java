428ab2d042c160f388dc4bd9f1813fee40c8d360
kimssser <skey2@inbox.lv>
2018-10-25 19:21:33 +0300

    final

--- src/test/java/FirstHomeWorkJUnit.java
+++ src/test/java/FirstHomeWorkJUnit.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 
 public class FirstHomeWorkJUnit {
 
-    // calculate total amount to be returned
     private double calcExpenses(double loanAmount) {
         double totalExpenses = loanAmount + loanAmount*0.10*10 + loanAmount*0.08*10 + loanAmount*0.06*10;
         System.out.println("Loan Amount: " + loanAmount + " | " + "Total Loan Expenses: "  + totalExpenses);
@@ -13,8 +12,8 @@ public class FirstHomeWorkJUnit {
 
     @Test
     public void compareAmounts(){
-        double initLoanAmount = 6000;  //specify loan amount
-        double totalAmountToReturn = 204020; //specify total amount to be returned
+        double initLoanAmount = 6000;
+        double totalAmountToReturn = 204020;
         Assertions.assertEquals(calcExpenses(initLoanAmount), totalAmountToReturn, "amount mismatch");
     }
 }
