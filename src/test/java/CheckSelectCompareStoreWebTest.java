8cacaf909c239a46448c354d9d01e0ccdc8738ba
kimssser <skey2@inbox.lv>
2018-11-11 22:39:40 +0200

    WebShop first simple draft version (not finished and not working)

--- src/test/java/CheckSelectCompareStoreWebTest.java
+++ src/test/java/CheckSelectCompareStoreWebTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.xml.bind.Element;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -14,11 +15,16 @@ public class CheckSelectCompareStoreWebTest {
 
     private final By WOMEN_BUTTON = By.xpath(".//ul[@class = 'sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li/a");
     private final By DRESSES_BUTTON = By.xpath(".//li[@class = 'last']/a");
-    private final By ORANGE_FILTER = By.xpath(".//ul[@class = 'col-lg-12 layered_filter_ul color-group']/li/label");
-    private final By SEARCH_BOX = By.xpath(".//*[@class = 'col-sm-4 clearfix']/form");
+    private final By ORANGE_INPUT =  By.xpath(".//ul[@class = 'col-lg-12 layered_filter_ul color-group']/li/input");
+    private final By ORANGE_FILTER = By.xpath(".//ul[@class = 'col-lg-12 layered_filter_ul color-group']/li/label/a");
+    private final By LOADING = By.xpath(".//ul[@class = 'product_list grid row']/p");
+    private final By SELECTED_EL_COUNT = By.xpath(".//*[contains(@class,'ajax_block_product col-xs-12')]");
+    private final By ELEMENT_COLORS = By.xpath(".//*[contains(@class,'ajax_block_product col-xs-12')]");
+
+
 
     @Test
-    public void compareArticles() {
+    public void compareArticles() throws InterruptedException {
 
         System.setProperty("webdriver.gecko.driver", "c:/geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
@@ -28,12 +34,29 @@ public class CheckSelectCompareStoreWebTest {
         women.get(0).click();
         List<WebElement> dresses = driver.findElements(DRESSES_BUTTON);
         dresses.get(1).click();
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-     //   wait.until(ExpectedConditions.visibilityOfElementLocated(SEARCH_BOX));
+        List<WebElement> input = driver.findElements(ORANGE_INPUT);
+        input.get(3).click();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(LOADING));
         List<WebElement> yellowFilter = driver.findElements(ORANGE_FILTER);
-        yellowFilter.get(4).click();
+        yellowFilter.get(3).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(LOADING));
+        List<WebElement> elementCount = driver.findElements(SELECTED_EL_COUNT);
+        List<WebElement> elementColors = driver.findElements(ELEMENT_COLORS);
+        int a = 0;
+        for (int i = 0; i < elementCount.size(); i++) {
+            for (int k = 0; k < elementColors.size(); k++) {
+                if (elementColors.get(k).getText().contains("color-orange")) {
+                    a = a + 1;
+                    break;
+                }
+            }
+            }
+
+
+
+
     }
 
 
 }
-
