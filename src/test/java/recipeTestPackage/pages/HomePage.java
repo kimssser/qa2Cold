6275bd3fddbe5f1a3cf19361633fb8ce2cbbb12f
kimssser <skey2@inbox.lv>
2019-01-06 21:07:00 +0200

    HomePage class minor update

--- src/test/java/recipeTestPackage/pages/HomePage.java
+++ src/test/java/recipeTestPackage/pages/HomePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 public class HomePage {
 
     private final By NAME = By.xpath(".//a[contains(@class, 'nav-link')]");
-    private recipeTestPackage.pages.BaseFunc baseFunc;
+    private BaseFunc baseFunc;
 
     public HomePage(BaseFunc baseFunc) {
 
