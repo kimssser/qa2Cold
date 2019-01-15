880c56fb365ce1b8d7b69b5b48b050a83c394236
kimssser <sergejs.kims@inbox.lv>
2019-01-15 15:53:45 +0200

    Updated and optimized

--- src/test/java/recipeTestPackage/RecipeTest.java
+++ src/test/java/recipeTestPackage/RecipeTest.java
@@ -2,7 +2,6 @@
 // go through all ingridients and check if this recipe exists on the ingridients page
 
 
-
 package recipeTestPackage;
 
 import org.junit.jupiter.api.Test;
@@ -14,7 +13,6 @@ import org.junit.jupiter.api.Assertions;
 
 public class RecipeTest {
     private final String HOME_PAGE = "delfi.lv";
-    private final String recipe_name = "Pufīgās sodas pankūciņas";
     private BaseFunc baseFunc = new BaseFunc();
 
     @Test
@@ -23,8 +21,8 @@ public class RecipeTest {
         HomePage homePage = new HomePage(baseFunc);
         homePage.acceptCookies();
         TastyPage tastyPage = homePage.getItemByName("TASTY");
-        RecipePage recipePage = tastyPage.getRecipe(recipe_name);
-        Integer recipeNotFoundCount  = recipePage.recipeIngredients();
+        RecipePage recipePage = tastyPage.getRecipe();
+        Integer recipeNotFoundCount = recipePage.recipeIngredients(tastyPage.getfirstRecipeName());
         Assertions.assertTrue(recipeNotFoundCount == 0, "Not all the ingredients exist in recipes on ingridient page");
     }
 }
