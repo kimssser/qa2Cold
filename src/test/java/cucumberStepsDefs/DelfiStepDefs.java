ea127ee7b755e586ed0f25cf955e88574f0b48db
kimssser <skey2@inbox.lv>
2019-01-27 16:21:13 +0200

    Cucumber test def

---
+++ src/test/java/cucumberStepsDefs/DelfiStepDefs.java
@@ -0,0 +1,10 @@
+package cucumberStepsDefs;
+
+import cucumber.api.java.en.Given;
+
+public class DelfiStepDefs {
+    @Given("Print test annotation (.*)")
+    public void print_test_annotation(String annotation) {
+        System.out.println(annotation);
+    }
+}
