56bb5aba6cc4d01d2da33d31a59220fa44f99177
kimssser <skey2@inbox.lv>
2019-01-27 16:23:17 +0200

    minor name changes

--- src/test/java/cucumberStepsDefs/TestStepDefs.java
+++ src/test/java/cucumberStepsDefs/TestStepDefs.java
@@ -2,7 +2,7 @@ package cucumberStepsDefs;
 
 import cucumber.api.java.en.Given;
 
-public class DelfiStepDefs {
+public class TestStepDefs {
     @Given("Print test annotation (.*)")
     public void print_test_annotation(String annotation) {
         System.out.println(annotation);
